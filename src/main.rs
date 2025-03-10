abba1b9f34e9aeb2a19f9b0c1abed2f64b284bdd
Arnold Emmanuel <arnoldemmanuel15@gmail.com>
2025-03-10 03:17:47 +0100

    update cli args

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,18 @@ mod services;
 struct Args {
     /// Ip address
     ip_addr: IpAddr,
+
+    /// Start of the range.
+    #[arg(short = 's', long, default_value_t = 1)]
+    start_port: u16,
+
+    /// End of the range of ports to scan (inclusive).
+    #[arg(short = 'e', long, default_value_t = 65535)]
+    end_port: u16,
+
+    /// Number of threads to use.
+    #[arg(short = 't', long, default_value_t = 4)]
+    threads: u16,
 }
 
 fn main() {
@@ -20,7 +32,7 @@ fn main() {
 
     let (tx, rx) = channel();
 
-    let new_scanner: Scanner = Scanner::new(args.ip_addr);
+    let new_scanner: Scanner = Scanner::new(args.ip_addr, args.threads, args.end_port);
 
     let threads = new_scanner.threads;
 
@@ -43,7 +55,7 @@ fn main() {
 
     open_ports.sort();
 
-    println!("\n"); // to format output
+    println!("\n"); // for output display
 
     for port in open_ports {
         println!("PORT: {port} is open")
--- src/services/scanner.rs
+++ src/services/scanner.rs
@@ -4,26 +4,26 @@ use std::{
     sync::mpsc::Sender,
 };
 
-const MAX: u16 = 65535; // max number of ports
-
 #[derive(Clone)]
 pub struct Scanner {
     ip_addr: IpAddr,
-    max: u16, // max no. of ports
+    end_port: u16, // max no. of ports
     pub threads: u16,
+    // start_port: u16,
 }
 
 impl Scanner {
-    pub fn new(ip_addr: IpAddr) -> Scanner {
+    pub fn new(ip_addr: IpAddr, threads: u16, end_port: u16) -> Scanner {
         Scanner {
             ip_addr,
-            max: MAX,
-            threads: 4,
+            end_port,
+            // start_port,
+            threads,
         }
     }
 
     pub fn scan(&self, start_port: u16, tx: Sender<u16>) {
-        let mut port: u16 = start_port + 1; // starting from port 1
+        let mut port: u16 = start_port + 1;
         loop {
             if TcpStream::connect((self.ip_addr, port)).is_ok() {
                 print!("."); // indicates a connection
@@ -31,7 +31,7 @@ impl Scanner {
                 tx.send(port).unwrap();
             }
 
-            if (self.max - port) <= self.threads {
+            if (self.end_port - port) <= self.threads {
                 break;
             }
             port += self.threads;
